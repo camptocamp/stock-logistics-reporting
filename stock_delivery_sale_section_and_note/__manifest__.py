364b502f09c0ce0f802383b9a487f0c563be58ed
Stephane Mangin <stephane.mangin@camptocamp.com>
2023-10-17 13:10:46 +0200

    [15.0][ADD] stock_delivery_sale_section_and_note

---
+++ stock_delivery_sale_section_and_note/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    "name": "Stock Delivery Sale Section and Note",
+    "description": "Stock Delivery Report Sale Section and Note Module",
+    "version": "15.0.1.0.0",
+    "author": "Camptocamp",
+    "license": "AGPL-3",
+    "category": "Delivery",
+    "depends": [
+        "delivery",
+        "sale_section_and_note_revamp",
+        "sale_stock",
+        "stock",
+    ],
+    "website": "http://www.camptocamp.com",
+    "data": [
+        "reports/report_deliveryslip.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+    "application": False,
+}
---
+++ stock_delivery_sale_section_and_note/tests/common.py
@@ -0,0 +1,15 @@
+# Copyright 2023 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo.addons.sale_section_and_note_revamp.tests.common import TestDisplayLineMixinCommon
+
+from odoo.tests import tagged
+
+
+@tagged('post_install', '-at_install')
+class TestStockDeliverySaleSectionNoteCommon(TestDisplayLineMixinCommon):
+    def setUp(self):
+        super().setUp()
+        self.sale_order.order_line._compute_product_updatable()
+        self.sale_order.action_confirm()
+        self.sale_order.order_line._compute_product_updatable()
