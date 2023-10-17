91108228d1fc57c6e4ea4f463d88fe0cefd465a3
Stephane Mangin <stephane.mangin@camptocamp.com>
2023-10-17 13:10:46 +0200

    [15.0][ADD] stock_delivery_sale_section_and_note

---
+++ stock_delivery_sale_section_and_note/models/stock_move.py
@@ -0,0 +1,37 @@
+# Copyright 2023 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+import datetime
+
+from odoo import fields, models
+
+
+class StockMove(models.Model):
+    _name = "stock.move"
+    _inherit = ["stock.move", "display.line.mixin"]
+
+    sequence = fields.Integer(related="sale_line_id.sequence", store=True)
+    previous_line_id = fields.Many2one(related="sale_line_id.previous_line_id")
+    next_line_id = fields.Many2one(related="sale_line_id.next_line_id")
+
+    def prepare_section_or_note_values(self, order_line):
+        """This method is intended to be used to `convert` a display line to
+        the current model
+
+        It is mainly used for display lines injection to delivery reports
+        """
+        self.ensure_one()
+        assert self.is_section_or_note()
+        related_order_line = self.sale_line_id
+        return {
+            "sequence": order_line.sequence,
+            "display_type": order_line.display_type,
+            "name": order_line.name,
+            "date": datetime.datetime.now(),
+            "company_id": order_line.company_id.id,
+            "product_id": related_order_line.product_id.id,
+            "product_uom_qty": 0,
+            "product_uom": related_order_line.product_uom.id,
+            "location_id": False,
+            "location_dest_id": False,
+            "procure_method": False,
+        }
